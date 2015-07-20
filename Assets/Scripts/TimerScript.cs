dbd8133de8a9a13c46ec0a71f7f691cc4c083bdf
Aniruddh Ravipati <ravipatianirudh@gmail.com>
2015-07-20 13:53:30 +0530

    Added timer

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+	private float startTime;
+	private Text text;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		text = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float currentTime = Time.time;
+		float difference = currentTime - startTime;
+		text.text = ((int)difference / 60).ToString () + " min " + ((int)difference % 60).ToString () + " sec ";
+		if (((int)difference / 60) == 12)
+			Debug.Log ("Time's up!");
+	
+	}
+}
